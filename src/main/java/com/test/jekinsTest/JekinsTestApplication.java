e9fa5c6a2bbe7a04f1dfb77177999130a8d4959e
YoonSeungKwon <113295771+YoonSeungKwon@users.noreply.github.com>
2024-08-25 19:07:25 +0900

    test

---
+++ src/main/java/com/test/jekinsTest/JekinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.jekinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JekinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JekinsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/jekinsTest/JekinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.jekinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
